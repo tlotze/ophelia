8daa32aac9f9b9d7495c6706315e91974dc4b354
Thomas Lotze <thomas@thomas-lotze.de>
2005-09-29 15:27:42 +0000

    move handling of url context attributes from pthandler to site
    --HG--
    extra : convert_revision : svn%3A284f7ec5-86ef-0310-ba8a-c943e10996c6/Ophelia/trunk%40124

--- pthandler.py
+++ pthandler.py
@@ -70,15 +70,6 @@ def handler(req):
 
     # initialize the environment
     context = Namespace()
-
-    site_prefix = req_options["SitePrefix"]
-    context.site_prefix = site_prefix
-
-    uri_path = req.parsed_uri[apache.URI_PATH]
-    if uri_path.endswith("/index.html"):
-        uri_path = uri_path[:-10]
-    context.absolute_url = site_prefix + uri_path
-
     slots = Namespace()
     macros = {}
 
