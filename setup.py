26f114bed5779a0bdbb18df6a6fc1d2c5df19523
Thomas Lotze <thomas@thomas-lotze.de>
2007-01-23 18:30:07 +0000

    eggified Ophelia
    --HG--
    extra : convert_revision : svn%3A284f7ec5-86ef-0310-ba8a-c943e10996c6/Ophelia/trunk%40218

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+#
+# Copyright (c) 2006-2007 Thomas Lotze
+# See also LICENSE.txt
+
+"""Create XHTML pages from templates written in Zope TAL.
+
+Ophelia creates XHTML pages from templates written in TAL, the Zope Tag
+Attribute Language. It is designed to reduce code repetition to zero.
+
+At present, Ophelia contains a request handler for the Apache2 web server.
+"""
+
+import os, os.path
+from setuptools import setup, find_packages
+
+
+def include_tree(dest, source):
+    source_len = len(source)
+    for dirpath, dirnames, filenames in os.walk(source):
+        yield (dest + dirpath[source_len:],
+               [os.path.join(dirpath, fn) for fn in filenames])
+        if ".svn" in dirnames:
+            dirnames.remove(".svn")
+
+desc, longdesc = __doc__.split("\n\n", 1)
+
+data_files = [
+    ("share/doc/Ophelia", [
+        "LICENSE.txt",
+        "COPYRIGHT.txt",
+        "README.txt",
+        "ROADMAP.txt",
+        "CHANGES.txt",
+        "doc/INSTALL.txt",
+        "doc/OVERVIEW.txt",
+        "doc/API.txt",
+        ]),
+    ] + list(include_tree("share/doc/Ophelia", "doc"))
+
+provides = [
+    "ophelia",
+    "opheliatools",
+    ]
+
+setup(name="Ophelia",
+      version="trunk",
+      description=desc,
+      long_description=longdesc,
+      author="Thomas Lotze",
+      author_email="thomas@thomas-lotze.de",
+      url="http://www.thomas-lotze.de/en/software/ophelia/",
+      license="ZPL 2.1",
+      packages=find_packages(),
+      include_package_data=True,
+      data_files=data_files,
+      provides=provides,
+      )
