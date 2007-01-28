411a6efd57f38499346dd1797b4f4da691b689a7
Thomas Lotze <thomas@thomas-lotze.de>
2007-01-28 23:16:12 +0000

    renaming methods to get rid of camel case and comply with PEP 8
    --HG--
    extra : convert_revision : svn%3A284f7ec5-86ef-0310-ba8a-c943e10996c6/Ophelia/trunk%40239

--- opheliatools/navigation.py
+++ opheliatools/navigation.py
@@ -13,9 +13,9 @@ class Navigation(object):
     def __init__(self, home=None):
         self.publisher = ophelia.publisher.get_publisher()
 
-        self.uri = self.uriFromSite(self.publisher.path)
+        self.uri = self.uri_from_site(self.publisher.path)
         if home is None:
-            self.home = self.uriFromSite("/")
+            self.home = self.uri_from_site("/")
         else:
             self.home = home
 
@@ -25,21 +25,21 @@ class Navigation(object):
         self.alt_langs = {}
 
     def set_chapter(self, title):
-        self.chapter = self.uriFromCurrent()
+        self.chapter = self.uri_from_current()
         self.chapter_title = title
 
-    def addMenu(self, entries, root_title=None, root=None):
-        self.menu[self.uriFromCurrent(root)] = (
-            [(self.uriFromCurrent(href), title) for href, title in entries],
+    def add_menu(self, entries, root_title=None, root=None):
+        self.menu[self.uri_from_current(root)] = (
+            [(self.uri_from_current(href), title) for href, title in entries],
             root_title)
 
-    def setBreadcrumb(self, title):
-        self.breadcrumbs[self.uriFromCurrent()] = title
+    def set_breadcrumb(self, title):
+        self.breadcrumbs[self.uri_from_current()] = title
 
-    def iterBreadcrumbs(self):
+    def iter_breadcrumbs(self):
         menu = {}
         for path in self.publisher.history:
-            uri = self.uriFromSite(path)
+            uri = self.uri_from_site(path)
             title = self.breadcrumbs.get(uri) or menu.get(uri)
             if title:
                 yield (uri, title)
@@ -47,31 +47,31 @@ class Navigation(object):
             if menu:
                 menu = dict(menu[0])
 
-    def conditionalLink(self, href, title):
+    def conditional_link(self, href, title):
         if href == self.uri:
             return title
         else:
             return '<a href="%s" title="Nav: %s">%s</a>' % \
                    (href, title, title)
 
-    def displayBreadcrumbs(self, sep):
-        bc = [self.conditionalLink(href, title)
-              for href, title in self.iterBreadcrumbs()]
+    def display_breadcrumbs(self, sep):
+        bc = [self.conditional_link(href, title)
+              for href, title in self.iter_breadcrumbs()]
         return sep.join(bc)
 
-    def displayMenu(self, depth=3, uri=None):
+    def display_menu(self, depth=3, uri=None):
         lines = ["<dl>"]
         if uri is None:
             uri = self.home
         root_title = self.menu[uri][1]
         if root_title:
-            lines.append("<dt>%s</dt>" % self.conditionalLink(
-                    self.uriFromPage(uri), root_title))
+            lines.append("<dt>%s</dt>" % self.conditional_link(
+                    self.uri_from_page(uri), root_title))
 
         def display(uri, deeper):
             for href, title in self.menu[uri][0]:
                 lines.append("<dt>%s</dt>" %
-                             self.conditionalLink(href, title))
+                             self.conditional_link(href, title))
                 if deeper and href in self.menu:
                     lines.append("<dd><dl>")
                     display(href, deeper-1)
@@ -81,25 +81,25 @@ class Navigation(object):
         lines.append("</dl>")
         return '\n'.join(lines)
 
-    def uriFromCurrent(self, path=None):
+    def uri_from_current(self, path=None):
         uri = urljoin(self.publisher.site, self.publisher.current)
         if path is not None:
             uri = urljoin(uri, path)
         return uri
 
-    def uriFromSite(self, path):
+    def uri_from_site(self, path):
         return urljoin(self.publisher.site, path)
 
-    def uriFromHome(self, path):
+    def uri_from_home(self, path):
         return urljoin(self.home, path)
 
-    def uriFromPage(self, path):
+    def uri_from_page(self, path):
         return urljoin(self.uri, path)
 
-    def uriFromUri(self, uri, path):
+    def uri_from_uri(self, uri, path):
         return urljoin(uri, path)
 
-    def altLangs(self, **kwargs):
+    def alt_langs(self, **kwargs):
         for lang, path_seg in kwargs.iteritems():
             self.alt_langs[lang] = urljoin(
-                self.alt_langs.get(lang, self.uriFromCurrent("")), path_seg)
+                self.alt_langs.get(lang, self.uri_from_current("")), path_seg)
