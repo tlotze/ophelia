df424933c93356d6254600698e38bf4b4a6c6bf6
Thomas Lotze <thomas@thomas-lotze.de>
2007-12-18 19:31:50 +0000

    cleaned up page template exceptions and file path reporting, raise on writing broken template text
    --HG--
    extra : convert_revision : svn%3A284f7ec5-86ef-0310-ba8a-c943e10996c6/Ophelia/trunk%40413

--- ophelia/pagetemplate.py
+++ ophelia/pagetemplate.py
@@ -8,10 +8,8 @@ class PageTemplateTracebackSupplement(object):
 
     def __init__(self, template):
         self.template = template
-
-    @property
-    def warnings(self):
-        return self.template._v_errors
+        self.source_url = template.file_path
+        self.warnings = template._v_errors
 
 
 class PageTemplate(zope.pagetemplate.pagetemplate.PageTemplate):
@@ -24,17 +22,14 @@ class PageTemplate(zope.pagetemplate.pagetemplate.PageTemplate):
 
     def __init__(self, text, file_path=None):
         super(PageTemplate, self).__init__()
-        self.write(text)
         self.file_path = file_path
+        self.write(text)
 
-    @property
-    def macros(self):
+    def _cook(self):
         __traceback_supplement__ = (PageTemplateTracebackSupplement, self)
-        macros = super(PageTemplate, self).macros
+        super(PageTemplate, self)._cook()
         if self._v_errors:
-            raise zope.pagetemplate.pagetemplate.PTRuntimeError(
-                "Can't compile template at %s." % self.file_path)
-        return macros
+            raise ValueError("There were errors in the page template text.")
 
     def pt_getContext(self, args=(), options=None, **ignored):
         return args[0]
