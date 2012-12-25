a673449043adcffa89103223d6d88188afb68b2b
Thomas Lotze <thomas@thomas-lotze.de>
2012-12-26 00:25:46 +0100

    declared the ``ophelia`` package a namespace
    --HG--
    branch : wsgi-only

--- ophelia/__init__.py
+++ ophelia/__init__.py
@@ -1,5 +1,1 @@
-# Copyright (c) 2006-2007 Thomas Lotze
-# See also LICENSE.txt
-
-"""Ophelia builds a web site from TAL templates with zero code repetition.
-"""
+__import__('pkg_resources').declare_namespace(__name__)
